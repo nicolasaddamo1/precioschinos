074c1fcaaff96b6b205087a93d58790ea9c269b1
Nico <nicolasaddamo1@gmail.com>
2024-07-11 10:16:54 -0300

    scrapper

---
+++ helper.js
@@ -0,0 +1,20 @@
+const puppeteer = require('puppeteer');
+
+async function getDataFromWebPage(){
+    const browser = await puppeteer.launch({
+        headless: false,
+        slowMo: 3000
+    });
+    const page = await browser.newPage();
+    await page.goto('https://ratoneando.ar/?q=a');
+
+    const result = await page.evaluate(() => {
+        const element = document.getElementsByClassName('sc-bqWxrE ivfBCn')[0];
+        return element ? element.outerHTML : null;
+    });
+
+    console.log(result);
+    await browser.close();
+}
+
+getDataFromWebPage();
